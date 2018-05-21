d11fd64b6bdf448c3c642a2153721bf977ffc9cb
kadotami <mondamin.kqfb@gmail.com>
2018-05-21 22:43:05 +0900

    add signup

--- db/migrate/20180521130926_create_users.rb
+++ db/migrate/20180521130926_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :password_digest
-      t.string :name
-      t.string :email
+      t.string :password_digest, :null => false
+      t.string :name, :null => false
+      t.string :email, :null => false
 
       t.timestamps
     end
+    add_index :users, [:email], unique: true
   end
 end
