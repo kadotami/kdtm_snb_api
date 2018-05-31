6639a90c0661c30757101a823f899fa0b9a5b412
kadotami <mondamin.kqfb@gmail.com>
2018-05-31 09:19:32 +0900

    modify signin url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
+  post 'signin' => 'user_token#create'
   resources :todos
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
